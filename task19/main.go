69a6a098915fd1ef2dd70973a12c618d90601aa1
neutral-jungle-creep <arrrseny11@gmail.com>
2023-02-11 20:02:56 +0300

    add task 19

---
+++ task19/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func reverseString(str string) string {
+	var (
+		runeLine    = []rune(str) // создание списка рун из строки
+		reverserStr string        // объявление переменной для записи результата
+	)
+
+	for i := len(runeLine) - 1; i >= 0; i-- { // счетчик устанавливается на индекс последнего элемента списка рун
+		reverserStr += string(runeLine[i]) // к результату прибавляется элемент списка рун от индекса, преобразованный в строку
+	}
+
+	return reverserStr
+}
+
+func main() {
+	var line string
+	if _, err := fmt.Scan(&line); err != nil { // получение от пользователя строки
+		log.Fatal(err.Error())
+	}
+
+	rLine := reverseString(line)
+
+	fmt.Printf("reversed line = %s", rLine)
+}
