3eeed834cff307fe69a3f09085c0006bdd8dc902
neutral-jungle-creep <arrrseny11@gmail.com>
2023-02-12 20:47:31 +0300

    add task 26

---
+++ task26/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"strings"
+)
+
+func checkForUniqueness(line string) bool {
+	var symbolsSet = map[rune]struct{}{}
+
+	for _, symbol := range []rune(strings.ToLower(line)) {
+		if _, exists := symbolsSet[symbol]; exists {
+			return false
+		} else {
+			symbolsSet[symbol] = struct{}{}
+		}
+	}
+	return true
+}
+
+func main() {
+	var line string
+	if _, err := fmt.Scan(&line); err != nil { // получение от пользователя строки
+		log.Fatal(err.Error())
+	}
+
+	fmt.Printf("[%s] --- %v ", line, checkForUniqueness(line))
+}
